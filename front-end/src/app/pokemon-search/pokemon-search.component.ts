38a6aa6277f14e17d4925023ab0aa0b6b0254c68
Gustavo Maldonado <maldonadogustavo55@gmail.com>
2021-11-19 20:25:16 +0100

    warning message if no pokemons match

--- front-end/src/app/pokemon-search/pokemon-search.component.ts
+++ front-end/src/app/pokemon-search/pokemon-search.component.ts
@@ -13,6 +13,8 @@ export class PokemonSearchComponent implements OnInit {
 
   pokemons$! : Observable<Pokemon[]>;
   private searchTerms = new Subject<string>();
+  noMatches = false;
+  activeSearch = false;
 
   constructor(
     private pokemonService: PokemonService
@@ -25,6 +27,10 @@ export class PokemonSearchComponent implements OnInit {
       switchMap((term: string) => this.pokemonService.
       searchPokemons(term))
     );
+      this.pokemons$.subscribe(x => x.length === 0? this.noMatches = true : this.noMatches = false);
+      this.searchTerms.subscribe(x => x.length !== 0? this.activeSearch = true : this.activeSearch = false);
+
+
   }
 
   search(term: string): void {
