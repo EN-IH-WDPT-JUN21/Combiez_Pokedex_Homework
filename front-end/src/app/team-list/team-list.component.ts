3a39a02f00b62ed2b5de06b6e2fdd45ea8f9cf44
Gustavo Maldonado <maldonadogustavo55@gmail.com>
2021-11-21 20:51:53 +0100

    Before change of type in getPokemonByName in PokemonService

--- front-end/src/app/team-list/team-list.component.ts
+++ front-end/src/app/team-list/team-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Team } from '../models/team';
+
+import { Result } from '../models/result.model';
+import { Team } from '../models/team.model';
+import { PokemonService } from '../service/pokemon.service';
 import { TeamService } from '../service/team.service';
 
 @Component({
@@ -9,16 +12,34 @@ import { TeamService } from '../service/team.service';
 })
 export class TeamListComponent implements OnInit {
 
-
   teams: Team[] = [];
+  teamPokemon : Result[] = [];
 
   constructor(
-    private teamService: TeamService
+    private teamService: TeamService,
+    private pokemonService: PokemonService
   ) { }
 
   ngOnInit(): void {
-    this.teamService.getTeams().subscribe(t => {
-      this.teams = t;
-    });
+    this.teamService.getTeams().subscribe(
+      teams => teams.forEach(
+        team => this.teams.push(
+          new Team(team.id, team.owner, team.pokemons.map(poke => {
+            let rObj = new Result('', '');
+            rObj.name = poke;
+            return rObj }))
+        )
+      )
+    );
+    this.getPokemonImages();
+    console.log(this.teams);
+  }
+
+  getPokemonImages(): void {
+    this.teams.forEach(
+      team => team.pokemons.forEach(
+        pokemon => this.pokemonService.getPokemonByName(pokemon.name)
+        .subscribe(poke => pokemon.url = poke.results[0].url)
+      ))
   }
 }
